3c1608ffbfbf1c43e58459955ab7e7c5addcd9f1
Eduardo Perottoni <edu.perottoni@gmail.com>
2020-12-12 14:04:53 -0300

    Ajustando o layout para responsividade mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
 const buttonChoose = document.querySelector('.main .logo-btn button');
 const peopleList = document.querySelector('.main .people-list')
-
+const foot = document.querySelector('footer')
 
 window.onload = function(){
     buttonChoose.addEventListener('click', function(){
         if(peopleList.style.display == 'flex'){
             peopleList.style.display = 'none';
+            foot.style.position = 'absolute';
+            foot.style.bottom = '0';
         }else{
             peopleList.style.display = 'flex';
+            foot.style.position = 'initial';
         }
 })}
