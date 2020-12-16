8081f4fe9da7a8e73d75ec2f34160ec79dad7db9
Eduardo Perottoni <edu.perottoni@gmail.com>
2020-12-15 21:19:34 -0300

    Corrigindo o script e finalizando a página inicial

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,25 @@
 const buttonChoose = document.querySelector('.main .logo-btn button');
 const peopleList = document.querySelector('.main .people-list')
 const foot = document.querySelector('footer')
+const main = document.querySelector('.main .container')
 
 window.onload = function(){
     buttonChoose.addEventListener('click', function(){
-        if(peopleList.style.display == 'flex'){
-            peopleList.style.display = 'none';
-            foot.style.position = 'absolute';
-            foot.style.bottom = '0';
+        if (window.matchMedia('(min-width:1100px)').matches){
+            if(peopleList.style.display == 'flex'){
+                peopleList.style.display = 'none';
+            }else{
+                peopleList.style.display='flex';
+            }
         }else{
-            peopleList.style.display = 'flex';
-            foot.style.position = 'initial';
-        }
-})}
+            if(peopleList.style.display == 'flex'){
+                peopleList.style.display = 'none';
+                foot.style.position = 'absolute';
+            }else{
+                peopleList.style.display = 'flex';
+                foot.style.position = 'initial';
+                peopleList.style.marginBottom = '20px';
+            }
+        }    
+    }
+)}
